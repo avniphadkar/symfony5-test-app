693e2a8233224372b317bcead52519b5db1022f3
Avinash Niphadkar <38523227+avniphadkar@users.noreply.github.com>
2024-06-28 19:03:54 +0530

    Update UserLocaleListener

--- EventListener/UserLocaleListener.php
+++ EventListener/UserLocaleListener.php
@@ -8,11 +8,8 @@ use TestApp\Entity\User;
 
 /**
  * Stores the locale of the user in the session after the login.
- * If no locale are defined (if user doesn't change it from the login screen), override it with the user's config one.
+ * If no locale is defined (if the user doesn't change it from the login screen), override it with the user's config one.
  *
- * This can be used by the LocaleListener afterwards.
- *
- * @see http://symfony.com/doc/master/cookbook/session/locale_sticky_session.html
  */
 class UserLocaleListener
 {
